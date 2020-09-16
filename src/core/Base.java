eb9a018dd7560bb52a3f857f2d0cc06cab1e8327
pipefake <70596191+pipefake@users.noreply.github.com>
2020-09-15 19:28:30 -0500

    encabezado

--- src/core/Base.java
+++ src/core/Base.java
@@ -1,8 +1,8 @@
 /**
- * Crea una copia segura …
+ * Base clase abstracta …
  *
  * @author Jonathan.rojas@uao.edu.co Jonathan Rojas Ibanez 2190039
- * @author andrea.perez@uao.edu.co Andrea Perez 2190936
+ * @author andrea.perez@uao.edu.co Andrea Lucia Perez Marquinez 2190527
  * @author juan_felipe.jimenez@uao.edu.co Juan Felipe Jimenez Salazar 2190038
  * @date 03 Septiembre 2020si
  * @version 1.0
